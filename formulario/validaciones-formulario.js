312f616d04d33a63554cd4bdee7b988f19d9775d
diane16sosa12@gmail.com <diane16sosa12@gmail.com>
2023-06-09 09:29:03 -0300

    Se completaron las validaciones del formulario

---
+++ formulario/validaciones-formulario.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+    $("#myForm").submit(function(event) {
+        //let name = $("#inputName").val();
+        //let lastName = $("#inputLastName").val();
+        let age = $("#inputAge").val();
+        let mail = $("#inputMail").val();
+        //let username = $("#inputUsername").val();
+        let password1 = $("#inputPassword1").val();
+        let password2 = $("#inputPassword2").val();
+        let error = false;
+
+        let ageFeedback = $("#inputAgeFeedback");
+        let emailFeedback = $("#inputEmailFeedback");
+        let passwordFeedback = $(".inputPasswordFeedback");
+
+        if (age < 0 || age > 150) {
+            error = true;
+
+            ageFeedback.show();
+        }
+
+        if (password1 !== password2) {
+            error = true;
+
+            passwordFeedback.show();
+        }
+
+        if (!/^\S+@\S+\.\S+$/.test(mail)) {
+            error = true;
+
+            emailFeedback.show();
+        }
+
+        if (error) {
+            event.preventDefault();
+            console.log("Formulario completado con errores");
+        }
+    })
+})
